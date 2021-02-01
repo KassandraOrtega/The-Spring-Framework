9ae699bf9210e75512ff8b55b9652c8cfcdc17e5
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-01 13:11:36 -0600

    Using spring framework to manage dependencies - @Component, @Autowired

--- spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,13 +2,23 @@ package com.kassandraortega.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+	// What are the beans? => Find using the @Component
+	// What are the dependencies of a bean? => Find using @Autowired
+	// Where to search for beans? => No need because we are in the same package.
 	public static void main(String[] args) {
 		
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		//int result = binarySearch.binarySearch(new int[] {124, 4, 6}, 3);
+		//System.out.println(result);
+		
+		// Application Context
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {124, 4, 6}, 3);
 		System.out.println(result);
 		
