5e0d19d4f8a328fe0b71d210727489aa6d757144
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-01 16:47:35 -0600

    Lifecyle of a bean - @PostConstruct, @PreDestroy

--- spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,15 +1,23 @@
 package com.kassandraortega.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import ch.qos.logback.classic.Logger;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 //Default is singleton, use scope to change it to prototype.
 public class BinarySearchImpl {
+	
+	private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	@Qualifier("bubble")
@@ -21,5 +29,15 @@ public class BinarySearchImpl {
 		System.out.println(sortAlgorithm);
 		// Search the array.
 		return 3;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("preDestroy");
 	}
 }
