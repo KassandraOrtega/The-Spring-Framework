8d0bc2e887dad9d384d249bee836766799e9c284
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-01 11:48:32 -0600

    Making the binary search algorithm loosely coupled.

--- spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -8,11 +8,11 @@ public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
 		
-		BinarySearchImpl binarySearch = new BinarySearchImpl();
+		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
 		int result = binarySearch.binarySearch(new int[] {124, 4, 6}, 3);
 		System.out.println(result);
 		
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
+		//SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
 }
