7e75a7665f1ddae979343ae43036d863aaed5eef
kassandradortega <kassandradortega@kassandras-mbp.attlocal.net>
2021-02-01 11:04:36 -0600

    Understanding tight coupling using the binary search algorithm example.

---
+++ spring-in-5-steps/src/main/java/com/kassandraortega/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,15 @@
+package com.kassandraortega.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		
+		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+		
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		// Implementing Sorting Logic
+		// Bubble Sort Algorithm
+		// Return the result
+		return 3;
+	}
+}
