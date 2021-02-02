2c1cb272a0d0b3b270289cdf6c6e38e21764f028
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-02 10:21:07 -0600

    Remove spring boot in basic application.Add logback/close app context.

--- spring-in-5-steps/src/test/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsBasicApplicationTests.java
+++ spring-in-5-steps/src/test/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsBasicApplicationTests.java
@@ -1,13 +1,16 @@
 package com.kassandraortega.spring.basics.springin5steps;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringIn5StepsBasicApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
