2b4057079e6156a257aba1943440badcd28b6ac1
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-02 16:55:16 -0600

    Spring Unit Testing with java and XML Context

--- spring-in-5-steps/src/test/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsBasicApplicationTests.java
+++ spring-in-5-steps/src/test/java/com/kassandraortega/spring/basics/springin5steps/SpringIn5StepsBasicApplicationTests.java
@@ -2,11 +2,10 @@ package com.kassandraortega.spring.basics.springin5steps;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 class SpringIn5StepsBasicApplicationTests {
 
 	@Test
---
+++ spring-in-5-steps/src/test/java/com/kassandraortega/spring/basics/springin5steps/basic/BinarySearchXMLConfigurationTest.java
@@ -0,0 +1,29 @@
+package com.kassandraortega.spring.basics.springin5steps.basic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kassandraortega.spring.basics.springin5steps.SpringIn5StepsBasicApplication;
+
+// Load the context
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations="/testContext.xml")
+class BinarySearchXMLConfigurationTest {
+
+	// Get this bean from the context
+	@Autowired
+	BinarySearchImpl binarySearch;
+	
+	@Test
+	public void testBasicScenario() {
+		int actualResult = binarySearch.binarySearch(new int[] {}, 5);
+		assertEquals(3, actualResult);
+		// Call method on binary search
+		// Check if the value is correct
+	}
+}
