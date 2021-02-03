7562fb7ae28d5f908dd608463a48a82029d9d121
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-03 13:00:48 -0600

    Execute the final method song CommandLineRunner

--- database-demo/src/main/java/com/kassandraortega/database/databasedemo/DatabaseDemoApplication.java
+++ database-demo/src/main/java/com/kassandraortega/database/databasedemo/DatabaseDemoApplication.java
@@ -1,12 +1,29 @@
 package com.kassandraortega.database.databasedemo;
 
+import java.lang.System.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kassandraortega.database.databasedemo.jdbc.PersonJbdcDao;
+
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJbdcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		((org.slf4j.Logger) logger).info("All users -> {}", dao.findAll());
 	}
 }
