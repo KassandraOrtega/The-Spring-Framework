ea84984a1900d60ce33777de4161fe5405dea78d
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-03 12:52:52 -0600

    Populate data into Person Table

---
+++ database-demo/src/test/java/com/kassandraortega/database/databasedemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassandraortega.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
