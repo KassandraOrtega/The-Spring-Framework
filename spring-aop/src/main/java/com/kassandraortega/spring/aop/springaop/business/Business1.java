a0f2b0f93d61f81d0fb6be108b2405578380b474
Kassandra Ortega <kassandraortega@kinandcarta.com>
2021-02-03 10:20:20 -0600

    Setting up AOP example

---
+++ spring-aop/src/main/java/com/kassandraortega/spring/aop/springaop/business/Business1.java
@@ -0,0 +1,18 @@
+package com.kassandraortega.spring.aop.springaop.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kassandraortega.spring.aop.springaop.data.Dao1;
+
+@Service
+public class Business1 {
+	
+	@Autowired
+	private Dao1 dao1;
+	
+	public String calculateSomething(){
+		//Business Logic
+		return dao1.retrieveSomething();
+	}
+}
